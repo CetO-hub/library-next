7660e3a19a59dcd2139de35a817ab0d1f6d47fd2
CetO-hub <skynet@SkyNet.localdomain>
2022-09-05 17:57:29 +0200

    Add initial set-up and logo

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+function MyApp({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
